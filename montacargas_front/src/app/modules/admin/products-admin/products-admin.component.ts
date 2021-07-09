ede46c3dc267f5c95a60ccd2328a05448bc52b14
Sebastian <posada.096@gmail.com>
2021-07-09 09:03:58 -0500

    users form

--- montacargas_front/src/app/modules/admin/products-admin/products-admin.component.ts
+++ montacargas_front/src/app/modules/admin/products-admin/products-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products-admin',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsAdminComponent implements OnInit {
 
-  constructor() { }
+  productsForm: FormGroup = this.fb.group({
+    id: [''],
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description : ['', Validators.required],
+    brand : ['', Validators.required],
+    size : ['', Validators.required],
+    state : ['', Validators.required],
+    reference : ['', Validators.required],
+    active: [true, [Validators.required]],
+    image : new FormArray([])
+  });
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
   }
-
+  get name() { return this.productsForm.get("name") } 
+  get price() { return this.productsForm.get("price") }  
+  get description() { return this.productsForm.get("description") }  
+  get brand() { return this.productsForm.get("brand") }  
+  get size() { return this.productsForm.get("size") }  
+  get state() { return this.productsForm.get("state") }  
+  get reference() { return this.productsForm.get("reference") }  
+  get active() { return this.productsForm.get("active") }  
+  get image() { return this.productsForm.get("image") }  
 }
